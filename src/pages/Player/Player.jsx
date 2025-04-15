f2a545b21b1517c50ed28c343e54e7fcafb4201e
Lakshan Jayaweera <127861567+LakshanJayaweera426@users.noreply.github.com>
2025-04-15 21:01:05 +0530

    d

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,8 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import "./Player.css";
+import back_arrow_icon from "../../assets/back_arrow_icon.png";
 
 const Player = () => {
-  return <div>Player</div>;
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const [apiData, setApiData] = useState({
+    name: "",
+    key: "",
+    published_at: "",
+    typeof: "",
+  });
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NmM0NDI1ZDA5MWU2NThjNTYzZWQwZWEwNDExNGNjMyIsIm5iZiI6MTc0NDcxMjg1Ni4wMzUsInN1YiI6IjY3ZmUzNDk4N2MyOWFlNWJjM2Q5OTYxMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hrpzijyVlAwfoBJ34Vt-O2NdW8YpLldtgV4nswqZkbE",
+    },
+  };
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`,
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => setApiData(res.results[0]))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div className="player">
+      <img
+        src={back_arrow_icon}
+        alt=""
+        onClick={() => {
+          navigate(-2);
+        }}
+      />
+      <iframe
+        width="90%"
+        height="90%"
+        src={`https://www.youtube.com/embed/${apiData.key}`}
+        title="trailer"
+        frameBorder="0"
+        allowFullScreen
+      ></iframe>
+      <div className="player-info">
+        <p>{apiData.published_at.slice(0, 10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Player;

04029da7a0f9b302546a44839287d7756134a0f1
Lakshan Jayaweera <127861567+LakshanJayaweera426@users.noreply.github.com>
2025-04-15 14:26:59 +0530

    Developed Title Card

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import hero_banner from "../../assets/hero_banner.jpg";
 import hero_title from "../../assets/hero_title.png";
 import play_icon from "../../assets/play_icon.png";
 import info_icon from "../../assets/info_icon.png";
+import TitleCards from "../../components/TitleCards/TitleCards";
 
 const Home = () => {
   return (
@@ -30,7 +31,14 @@ const Home = () => {
               More Info
             </button>
           </div>
+          <TitleCards />
         </div>
+      </div>
+      <div className="more-cards">
+        <TitleCards title={"Blockbuster Movies"} />
+        <TitleCards title={"Only on Netflix"} />
+        <TitleCards title={"Upcomming"} />
+        <TitleCards title={"Top Pics for You"} />
       </div>
     </div>
   );
